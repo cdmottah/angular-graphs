a691af8ec9e1e5ec5ec8da4efd6cc404614e8708
Cristian Danilo Motta Herrera <cdmottah@gmail.com>
2022-12-29 21:27:58 -0500

    version inicial barras

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
-  title = 'angular-graphs';
+  x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+  y = [1, 5, 7, 9, 3, 4, 7, 15, 20, 6, 11, 30]
+  Xlabel = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+
 }
